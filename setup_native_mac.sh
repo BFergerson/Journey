7368d3d7d4da97edf6ccbef995018751bed689ce
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-05-27 08:21:08 -0600

    test different build

--- setup_native_mac.sh
+++ setup_native_mac.sh
@@ -7,6 +7,5 @@ git checkout d348788e3347fa4d2a421773463f7dd62da60991
 cd .. && ./gradlew patchJCEF
 
 cd jcef && mkdir jcef_build && cd jcef_build
-cmake -G "Unix Makefiles" -DPROJECT_ARCH="x86_64" -DCMAKE_BUILD_TYPE=Release ..
-make -j4
-
+cmake -G "Xcode" -DPROJECT_ARCH="x86_64" -DCMAKE_BUILD_TYPE=Release ..
+xcodebuild -project jcef.xcodeproj -configuration Release -target ALL_BUILD
