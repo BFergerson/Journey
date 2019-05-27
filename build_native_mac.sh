79b0e9b7e97903b7ed3ad43d8d1694ed7316b497
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-05-27 07:35:29 -0600

    fix mac setup

--- build_native_mac.sh
+++ build_native_mac.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 cd jcef/tools && ./make_distrib.sh macosx64
-cd ../binary_distrib && strip ./macosx64/bin/lib/linux64/libcef.so
+cd ../binary_distrib && strip ./macosx64/bin/lib/macosx64/libcef.so
 zip -r jcef-distrib-macosx64.zip macosx64
