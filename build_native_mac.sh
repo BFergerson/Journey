039e61dd33df74a76fe4705809d2b611cec69b67
Brandon Fergerson <fergerson92@gmail.com>
2019-06-04 19:25:37 -0600

    install_name_tool

--- build_native_mac.sh
+++ build_native_mac.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+cd jcef/jcef_build/native/Release
+install_name_tool -change "@rpath/Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework" "@loader_path/../Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework" ./jcef_app.app/Contents/Java/libjcef.dylib
+install_name_tool -change "@rpath/Frameworks/Chromium Embedded Framework.framework/Chromium Embedded Framework" "@executable_path/../../../Chromium Embedded Framework.framework/Chromium Embedded Framework" "./jcef_app.app/Contents/Frameworks/jcef Helper.app/Contents/MacOS/jcef Helper"
+cd ../../../../
+
 cd jcef/tools && ./make_distrib.sh macosx64
 cd ../binary_distrib
 zip -r jcef-distrib-macintosh64.zip macosx64
