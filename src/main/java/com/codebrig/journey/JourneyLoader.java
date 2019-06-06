d18be30163682fb1a743f5da3a712f40b6c316cc
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-05 20:23:53 -0600

    version bump

--- src/main/java/com/codebrig/journey/JourneyLoader.java
+++ src/main/java/com/codebrig/journey/JourneyLoader.java
@@ -23,7 +23,7 @@ import java.util.zip.ZipFile;
  * Downloads and loads the necessary CEF files for the current OS.
  *
  * @author <a href="mailto:brandon.fergerson@codebrig.com">Brandon Fergerson</a>
- * @version 0.2.2
+ * @version 0.2.3
  * @since 0.1.1
  */
 public class JourneyLoader {
