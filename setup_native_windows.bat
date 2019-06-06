d11df0bf214f82df02886c05781d4e7ef649ffa8
Brandon Fergerson <brandon.fergerson@codebrig.com>
2019-06-05 21:34:01 -0600

    fix windows build

--- setup_native_windows.bat
+++ setup_native_windows.bat
@@ -1,7 +1,7 @@
 SET PATH=%JAVA_HOME%;%PATH%
 
 git clone https://bitbucket.org/chromiumembedded/java-cef.git jcef
-gradlew.bat checkoutJCEF
+call gradlew.bat checkoutJCEF
 
 echo "Running cmake..."
 cd jcef
